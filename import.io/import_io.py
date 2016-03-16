e7071470320da05812cd3c45fe9f44b73efa2552
CharlieFig1 <cherrera@gmail.com>
2016-03-15 23:01:49 -0400

    add header

---
+++ import.io/import_io.py
@@ -0,0 +1,2 @@
+r = requests.get('https://api.import.io/store/connector/f3de7739-a4b3-43e4-b76a-51dbda6c42bb/_query?input=webpage/url:http%3A%2F%2Fdmz.ryerson.ca%2Fteams%2Fjobs-opportunities%2F&&_apikey=a4480bc190b9471bbf6093643530305ea50a4de4c3fb6b594ce01d433ab157e991b5044184d1b259f885f52b46c08acfa890cf016617f03bd52066de59490191fadffb0837c294ab821b8e8700508d0e')
+
